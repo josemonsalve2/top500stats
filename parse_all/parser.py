3de5667d4b71951b9d32ec8f28f3be55ed35249e
Jose M Monsalve Diaz <josem@udel.edu>
2018-10-05 11:23:09 -0500

    Adding caching mechanism in case the program breaks. Adding exception handling for the requests.get function Adding -low -high -parser-cache-rate -parser-cache options Other small changes

--- parse_all/parser.py
+++ parse_all/parser.py
@@ -5,12 +5,26 @@ import functools
 import re
 import json
 import argparse
+import os
 
+# Some config parameters 
 DEBUG = False
+TEMP_CACHE_FILENAME="_temp_cache.json"
+FAILED_REPORT_FILENAME="failed_req.err"
+DEFAULT_OUTPUT_FILENAME="out.json"
 
+#Helper functions
 def log(string):
     if (DEBUG):
         print (string)
+
+def checkpoint(systems):
+    try:
+        with open(TEMP_CACHE_FILENAME,"w") as json_file:
+            json.dump(systems, json_file)
+    except:
+        print("I could not save the cache file for some reason")
+
 
 ''' 
 Class that contains the parsing methods depending
@@ -24,10 +38,19 @@ class top500Parser:
     ''' 
     Obtain a list of all systems
     '''
-    def parseSystems(self, topNum=999999):
+    def parseSystems(self, systems=None, bottomNum=1, topNum=999999, cacheRate=1000):
         all_the_systems = []
+        if systems is not None:
+            all_the_systems = systems
+
+        failed_requests = []
         for i in range(1,topNum):
-            response = requests.get(self.base_url+'system/'+str(i))
+            try:
+                response = requests.get(self.base_url+'system/'+str(i))
+            except Exception as e:
+                log(f"Error fetching system {i}: {str(e)}")
+                failed_requests.append(i)
+                continue
             html_doc = response.text
             soup = bs(html_doc, 'html.parser')
             # Check if the system returned an invalid website
@@ -82,7 +105,18 @@ class top500Parser:
                 # Append the new system to all the systems list
                 all_the_systems.append(system_params)
             # end if
+            if (cacheRate is not 0 and i%cacheRate is 0):
+                checkpoint(all_the_systems)
         # end for
+        
+        # Dump failed requests
+        if (len(failed_requests) is not 0):
+            try: 
+                with open(FAILED_REPORT_FILENAME,"w") as fp:
+                    for f_req in failed_requests:
+                        fp.write("%s\n" % str(f_req))
+            except:
+                log("Error logging failed requests. Could not create the file")
         # Return 
         return all_the_systems
 
@@ -92,8 +126,12 @@ def main():
     ## Select the phase options
     parser = argparse.ArgumentParser(description='This script helps parsing the top 500 website and analysing')
 
-
     parser.add_argument("-debug","-d", action="store_true", help="Add debugging messages")
+    parser.add_argument("-low","-ll", type=int, default=0, help="Lower limit for system search")
+    parser.add_argument("-high","-hl", type=int, default=999999, help="Upper limit for system search")
+    parser.add_argument("-parser-cache-rate","-cr", default=1000, type=int, help="Rate at which an snapshot is saved")
+    parser.add_argument("-parser-cache","-lc", help="file to load previously parsed systems")
+    parser.add_argument("-output-filename","-o", default=DEFAULT_OUTPUT_FILENAME, help="Filename where the output will be stored (out.json)")
 
     group = parser.add_mutually_exclusive_group()
     group.add_argument('-parse-website', action='store_true', help='Enable website parsing')
@@ -111,14 +149,26 @@ def main():
 
     #use the parser
     if (args.parse_website):
+        if (args.parser_cache):
+            try:
+                with open(args.parser_cache, "r") as fp:
+                    systems = json.load(json_file)
+                    log("Cache file loaded")
+            except:
+                print("I could not open the cache file. Problem openning the specified file")
         print("This will take some time ...")
         parser = top500Parser()
-        systems = parser.parseSystems()
+        systems = parser.parseSystems(systems, args.low, args.high, args.parser_cache_rate)
         try:
-            with open("out.json","w") as json_file:
+            with open(args.output_filename,"w") as json_file:
                 json.dump(systems, json_file)
-        except:
+                # Deleting possible temp file
+                if (os.path.isfile(TEMP_CACHE_FILENAME)):
+                    os.remove(TEMP_CACHE_FILENAME)
+                    pass
+        except Exception as e:
             print("I could not save the file for some reason")
+            log(f"ERROR: {str(e)}")
 
     if (args.load_file):
         try:
@@ -126,7 +176,6 @@ def main():
             with open(args.load_file,"r") as json_file:
                 systems = json.load(json_file)
                 log("Systems loaded")
-
         except:
             print(f"The file {args.load_file} does not exist or cannot be open") 
 
